6550de009a9d6b2894255f4bb80146c25f94eaf7
Pavlos Nikou <103177675+pavlos-nikou@users.noreply.github.com>
2024-03-26 10:30:26 +0200

    Delete lab2/lab2 directory

--- lab2/lab2/Lab02_1.py
+++
@@ -1,4 +0,0 @@
-DISCOUNT = 0.2
-price = float(input("please provide the items price:"))
-discountedPrice = price - price * DISCOUNT
-print(f'The sale price is {discountedPrice:.1f}')
--- lab2/lab2/Lab02_2.py
+++
@@ -1,6 +0,0 @@
-score_1 = float(input("Enter the first test score:"))
-score_2 = float(input("Enter the second test score:"))
-score_3 = float(input("Enter the third test score:"))
-
-averageScore = (score_1 + score_2 + score_3)/3
-print(f'The average score is {averageScore:.1f}')
