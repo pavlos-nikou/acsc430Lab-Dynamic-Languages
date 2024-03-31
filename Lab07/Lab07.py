96c6608bf1046bbfce2444dc1edb2d0d46c9164f
pavlos <pavlosn.pn@gmail.com>
2024-03-31 16:38:43 +0300

    progress was made today

--- Lab07/Lab07.py
+++ Lab07/Lab07.py
@@ -2,6 +2,52 @@ import matplotlib
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("sales.csv")
+def get_yearly_average(df):
+    salesData = df.drop(["month_number", "total_units", "total_profit"],axis=1)
+    return salesData.mean()
 
-print(df.to_string())
+
+commands = [
+    [1, "Print month with the biggest profit"],
+    [2, "Print average sales for all products"],
+    [3, "Print records with toothpaste sales > 6000 and face cream sales < 3000"],
+    [4, "Line plot of total profit for all months (X: Month, Y: profit)"],
+    [5, "Multiline plot of units sold per month for each product"],
+    [6, "Scatterplot showing correlation between face cream and shampoo sales"],
+    [7, "Bar chart of shampoo sales for all months"],
+    [8, "Calculate yearly average sales of moisturizer using DataFrame.to_numpy()"],
+    [9, "Line plot of total profit for all months"],
+    [10, "Exit"]
+]
+
+df = pd.read_csv("sales.csv")
+print("Welcome!!\nsales.csv was loaded successfully")
+
+
+
+while True:
+    print("Command\t\tCommand Description\n")
+    for command in commands:
+        print(f"{command[0]}\t\t{command[1]}")
+    inputCommand = input("command:")
+    if inputCommand == "1":
+        continue
+    if inputCommand == "2":
+        continue
+    if inputCommand == "3":
+        continue
+    if inputCommand == "4":
+        continue
+    if inputCommand == "5":
+        continue
+    if inputCommand == "6":
+        continue
+    if inputCommand == "7":
+        continue
+    if inputCommand == "8":
+        continue
+    if inputCommand == "9":
+        continue
+    if inputCommand == "10":
+        print("Goodby!!")
+        break
---
+++ Lab07/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+while True:
+#     print("Commands:")
+#     print("1 : get the month with biggest profit\n")
